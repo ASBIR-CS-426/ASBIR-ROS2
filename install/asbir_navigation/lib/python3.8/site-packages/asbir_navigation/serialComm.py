149c0d50adffa4fd76031c5a972b1dd1a6bd8eed
Tanner <trichnak72399@gmail.com>
2023-03-02 13:50:47 -0800

    serial communication

--- install/asbir_navigation/lib/python3.8/site-packages/asbir_navigation/serialComm.py
+++ install/asbir_navigation/lib/python3.8/site-packages/asbir_navigation/serialComm.py
@@ -2,10 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
-
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
+from std_msgs.msg import Int32MultiArray
 
 import serial
 
@@ -13,15 +10,45 @@ import serial
 class SerialComm(Node):
     def __init__(self):
         super().__init__('serialComm')
-        self.tfBuffer = Buffer()
-        self.tfListener = TransformListener(self.tfBuffer, self)
-        self.ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
+        self.ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, write_timeout=1)
+        # self.control = "1500 1500 1600 1600"
+        self.control = "1500 1500 1600 1600\n"
         
-        self.servoSub = self.create_subscription(Float32MultiArray, 'servoControl', self.servoSubCallback, 1)
+        self.timer = self.create_timer(0.2, self.broadcast_timer_callback)
+        self.servoSub = self.create_subscription(Int32MultiArray, 'servoControl', self.servoSubCallback, 1)
+        
+    def broadcast_timer_callback(self):
+        try:
+            self.ser.write(self.control.encode())
+            print(self.control)
+        except serial.serialutil.SerialTimeoutException:
+            print("timeout")
+
 
     def servoSubCallback(self, msg):
-        self.ser.write('{} {} {} {}'.format(msg.data[0],msg.data[1],msg.data[2],msg.data[3]))
-        print('{} {} {} {}'.format(msg.data[0],msg.data[1],msg.data[2],msg.data[3]))
+        (fs,bs,fw,bw) = (msg.data[0],msg.data[1],msg.data[2],msg.data[3])
+        if fs < 1000:
+            fsS = ("0%d" %fs)
+        else:
+            fsS = ("%d" %fs)
+
+        if bs < 1000:
+            bsS = ("0%d" %bs)
+        else:
+            bsS = ("%d" %bs)
+
+        if fw < 1000:
+            fwS = ("0%d" %fw)
+        else:
+            fwS = ("%d" %fw)
+        
+        if bw < 1000:
+            bwS = ("0%d" %bw)
+        else:
+            bwS = ("%d" %bw)
+
+        self.control = ("%s %s %s %s\n" %(fsS,bsS,fwS,bwS))
+        
 
 def main(args=None):
     rclpy.init(args=args)
